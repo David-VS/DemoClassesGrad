68e72acf57f2f84741fba5add41d06ca310b1163
David-VS <63110701+David-VS@users.noreply.github.com>
2024-11-18 15:17:52 +0100

    clean up static and final examples in class cirkel

--- src/be/ehb/demo/Main.java
+++ src/be/ehb/demo/Main.java
@@ -3,10 +3,14 @@ package be.ehb.demo;
 import be.ehb.demo.vormen.Cirkel;
 import be.ehb.demo.vormen.Driehoek;
 
+import java.net.http.HttpResponse;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        final float PI = 3.1415f;
 
         String tekst = new String("nieuwe tekst");
         Driehoek driehoek = new Driehoek(3, 3);
@@ -15,8 +19,11 @@ public class Main {
 
         Cirkel myCirkel = new Cirkel(4);
         System.out.println(myCirkel.oppervlakte());
+        System.out.println(Cirkel.teller);
         Cirkel uwCirkel = new Cirkel(60);
         System.out.println(uwCirkel.oppervlakte());
+        System.out.println(Cirkel.teller);
+        System.out.println(Cirkel.PI);
 
 
     }
--- src/be/ehb/demo/vormen/Cirkel.java
+++ src/be/ehb/demo/vormen/Cirkel.java
@@ -3,13 +3,18 @@ package be.ehb.demo.vormen;
 public class Cirkel {
 
     int straal;
+    public static int teller = 0;
+    public final static float PI = 3.1415f;
 
     public Cirkel(int straal){
         this.straal = straal;
+        teller++;
     }
 
     public float oppervlakte(){
-        return this.straal * this.straal * 3.14f;
+        return this.straal * this.straal * PI;
     }
+
+
 
 }
